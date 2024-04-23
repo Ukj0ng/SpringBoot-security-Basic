735d4ea8a290f9b38a4b6527ae4b1daa6d6a15cb
Ukjong <xb253q@gmail.com>
2024-04-23 20:29:49 +0900

    feat: user, manager, admin, login, join, joinProc에 대한 매핑 구현

---
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.security1.config;public class SecurityConfig {
+}
--- src/main/java/com/example/security1/controller/IndexController.java
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴하겠다
 public class IndexController {
@@ -12,6 +13,38 @@ public class IndexController {
     public String index() {
         // thymeleaf 기본폴더 src/main/resources/
         // view resolve 설정: templates(prefix), .html(suffix)
-        return "index.html";
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    // spring-security가 login을 가로챔
+    // but SecurityConfig파일을 만들고 나니 작동을 안함
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨!";
     }
 }
