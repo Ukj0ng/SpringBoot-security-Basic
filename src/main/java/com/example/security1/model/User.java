81dabc2effbfc24c9e6d87cf8b065e8f31c7c229
Ukjong <xb253q@gmail.com>
2024-04-23 21:26:38 +0900

    feat: User 모델 추가

--- src/main/java/com/example/security1/model/User.java
+++ src/main/java/com/example/security1/model/User.java
@@ -1,2 +1,24 @@
-package com.example.security1.model;public class User {
+package com.example.security1.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.sql.Timestamp;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    @CreationTimestamp
+    private Timestamp createDate;
 }
