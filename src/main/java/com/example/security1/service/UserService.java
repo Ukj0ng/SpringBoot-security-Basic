4d771b94384967951fbf8a4495f4c9b951b3abc0
Ukjong <xb253q@gmail.com>
2024-04-23 21:31:26 +0900

    feat: 회원가입 구현 - user의 비밀번호 암호화 - db에 save

--- src/main/java/com/example/security1/service/UserService.java
+++ src/main/java/com/example/security1/service/UserService.java
@@ -1,2 +1,31 @@
-package com.example.security1.service;public class UserService {
+package com.example.security1.service;
+
+import com.example.security1.model.User;
+import com.example.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public int 회원가입(User user) {
+        String rawPassword = user.getPassword();
+        String encPassword = passwordEncoder.encode(rawPassword);
+        user.setRole("ROLE_USER");
+        user.setPassword(encPassword);
+        try {
+            userRepository.save(user);
+            return 1;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
 }
