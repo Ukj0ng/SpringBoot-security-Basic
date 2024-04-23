74008f142d996c84f8571369fe936fd95190b3fb
Ukjong <xb253q@gmail.com>
2024-04-23 21:30:37 +0900

    feat: jpa repository 생성

--- src/main/java/com/example/security1/repository/UserRepository.java
+++ src/main/java/com/example/security1/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.example.security1.repository;public interface UserRepository {
+package com.example.security1.repository;
+
+import com.example.security1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// CRUD 함수를 JpaRepository가 들고 있음.
+// @Repository라는 어노테이션이 없어도 IoC가 됨 -> JpaRepository를 상속받았기 때문에
+public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
