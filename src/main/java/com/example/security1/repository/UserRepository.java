1d9f0610ce6cba2240db5b413e71661f281bbb9a
Ukjong <xb253q@gmail.com>
2024-04-23 22:17:58 +0900

    feat: Jpa query method 추가 - username으로 entity 찾기

--- src/main/java/com/example/security1/repository/UserRepository.java
+++ src/main/java/com/example/security1/repository/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // CRUD 함수를 JpaRepository가 들고 있음.
 // @Repository라는 어노테이션이 없어도 IoC가 됨 -> JpaRepository를 상속받았기 때문에
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    // findBy규칙 -> Username, Jpa query methods
+    // select * from user where username = 1?
+    User findByUsername(String username);
 }
