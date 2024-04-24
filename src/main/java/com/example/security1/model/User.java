58e238f61e2d0534dbea7ebded6e2b56592e5211
Ukjong <xb253q@gmail.com>
2024-04-24 23:24:16 +0900

    feat: 어노테이션 추가- @Builder - @AllArgsConstructor - @NoArgsConstructor

--- src/main/java/com/example/security1/model/User.java
+++ src/main/java/com/example/security1/model/User.java
@@ -5,13 +5,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
