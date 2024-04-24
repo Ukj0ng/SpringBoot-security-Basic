f5da7ba592a163654fd728ca2cea57a248ab4ea4
Ukjong <xb253q@gmail.com>
2024-04-24 21:15:30 +0900

    feat: google login 연결

--- src/main/java/com/example/security1/config/SecurityConfig.java
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.security1.config;
 
+import com.example.security1.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity // spring-security filter(여기서는 SecurityConfig)가 spring filter chain에 등록됨
 @EnableMethodSecurity (securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 활성화
 public class SecurityConfig{
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
+
 
     // Bean을 붙히면 해당 메서드의 리턴되는 오브젝트를 IoC에 등록해준다.
     @Bean
@@ -37,6 +42,14 @@ public class SecurityConfig{
             )
             .oauth2Login(oauth -> oauth
                 .loginPage("/loginForm")
+            )
+            // 1. 코드받기(인증), 2. 엑세스토큰(권한),
+            // 3. 사용자프로필 정보를 가져오기, 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+            // 4-2. (email, phone number, name, id) -> 받은 정보에 없는 추가적인 정보가 필요함
+            .oauth2Login(oauth -> oauth
+                .loginPage("/loginForm") // 구글 로그인 왼료된 뒤에 후처리가 필요함. Tip. 코드x, (엑세스토큰 + 사용자프로필 정보O)
+                .userInfoEndpoint(userInfo -> userInfo
+                    .userService(principalOauth2UserService))
             );
         return http.build();
     }
