e088ea18f08e7cc143fa20ee91b98d0dfc0087ac
Ukjong <xb253q@gmail.com>
2024-04-25 13:40:41 +0900

    feat: GoogleUserInfo 생성

--- src/main/java/com/example/security1/oauth/provider/GoogleUserInfo.java
+++ src/main/java/com/example/security1/oauth/provider/GoogleUserInfo.java
@@ -1,2 +1,32 @@
-package com.example.security1.oauth.provider;public class GoogleUserInfo {
+package com.example.security1.oauth.provider;
+
+import java.util.Map;
+
+public class GoogleUserInfo implements OAuth2UserInfo {
+
+    private Map<String, Object> attributes;  // oauth2User.getAttributes()
+
+    public GoogleUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return this.attributes.get("sub").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "google";
+    }
+
+    @Override
+    public String getEmail() {
+        return this.attributes.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return this.attributes.get("name").toString();
+    }
 }
