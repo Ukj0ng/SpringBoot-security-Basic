69121a037f037240bd89b8c13da5a7ec15815b19
Ukjong <xb253q@gmail.com>
2024-04-25 13:40:52 +0900

    feat: NaverUserInfo 생성

--- src/main/java/com/example/security1/oauth/provider/NaverUserInfo.java
+++ src/main/java/com/example/security1/oauth/provider/NaverUserInfo.java
@@ -1,2 +1,32 @@
-package com.example.security1.oauth.provider;public class NaverUserInfo {
+package com.example.security1.oauth.provider;
+
+import java.util.Map;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+
+public class NaverUserInfo implements OAuth2UserInfo{
+    private Map<String, Object> attributes;  // oauth2User.getAttributes()
+
+    public NaverUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return this.attributes.get("id").toString();
+    }
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getEmail() {
+        return this.attributes.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return this.attributes.get("name").toString();
+    }
 }
